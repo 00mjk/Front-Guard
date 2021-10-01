fc3dfdca9fc2a6cabcf1bdefda9628cf5d80e9f5
jocarrd <miguel@jig.es>
2021-10-01 09:22:47 +0000

    inicio

---
+++ src/components/CardMap.vue
@@ -0,0 +1,50 @@
+<template>
+  <q-card class="my-card">
+    <q-card-section   class="headercardmap"  >
+      <img src="../assets/BusStop.svg" />
+     
+      <p  style="display:inline;color: #FFFFFF;" class="p-body1">{{Header}} </p>
+    </q-card-section>
+    <q-card-section>
+      <p class="p-body2" style="font-size: 12px; color: #282828;">NEXT STOP</p>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+    props: {
+      Header: String ,
+      
+      
+  },
+  setup() {},
+});
+</script>
+
+<style scoped>
+ .headercardmap{
+  
+height: 48px;
+
+
+/* Bus Lines/201 */
+
+background: #D41217;
+/* Shadow/Soft */
+
+box-shadow: 0px 8px 16px rgba(17, 17, 17, 0.04);
+border-radius: 12px 12px 0px 0px;
+
+
+/* Bus Lines/201 */
+
+background: #D41217;
+/* Shadow/Soft */
+
+box-shadow: 0px 8px 16px rgba(17, 17, 17, 0.04);
+border-radius: 12px 12px 0px 0px;
+ }
+</style>
---
+++ src/pages/TripPlanner.vue
@@ -0,0 +1,86 @@
+<template>
+  <q-page
+    class="doc-page"
+    style="max-width: 1440px; margin: 0 auto; min-height: 0px; padding: 16px"
+  >
+    <div class="row q-col-gutter-md">
+      <div class="col">
+        <q-breadcrumbs
+          gutter="none"
+          style="
+            font-family: Source Sans Pro;
+            font-style: normal;
+            font-weight: normal;
+            font-size: 12px;
+            line-height: 15px;
+            color: #282828;
+          "
+        >
+          <q-breadcrumbs-el label="HOME / TRIP PLANNER" />
+        </q-breadcrumbs>
+      </div>
+    </div>
+
+    <div class="row q-col-gutter-md" style="margin-top: 61px">
+      <div class="col-md-2 col-sm-6 q-mb-lg">
+        <h4>Trip planner<span style="color: #d92127">.</span></h4>
+        <p class="p-body2">Routes from Monday to Saturday</p>
+      </div>
+    </div>
+
+    <div class="row q-col-gutter-md" style="margin-top: 8px">
+      <div class="col-md-3 col-sm-6 col-xs-12 q-mb-lg">
+        <input-generico-home
+          Header="From"
+          Hint="Write the name of the street, bus stop or your location "
+          Label="Leonardo Hotel Wolfsburg City Center"
+        />
+        <input-generico-home
+          Label="Volkswagen AutoMuseum"
+          Hint="Write the name of the street or bus stop"
+          Header="To"
+          style="margin-top: 16px"
+        />
+        <p class="p-body2" style="color: #9a9a9a; margin-top: 16px">
+          MORE OPTIONS
+        </p>
+      </div>
+
+      <div class="col-md-9 col-sm-12 col-xs-12 q-mb-lg">
+        <div class="mapa">
+          <div class="q-pa-md">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11912.084941383731!2d10.801808852063795!3d52.426292973821795!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47af93c8d01e69e5%3A0xe2465df60ba210ae!2sVolkswagen-Arena!5e0!3m2!1ses!2ses!4v1632827773646!5m2!1ses!2ses"
+              allowfullscreen=""
+              loading="lazy"
+              key=""
+              style="min-height: 849px; border: 0; height: 100%; width: 100%"
+            ></iframe>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-4 col-sm-12 col-xs-12 q-mb-lg q-ma-xl">
+              <card-map Header="Rathaus"/>
+          </div>
+
+          <div class="col-md-4 col-sm-12 col-xs-12 q-mb-lg q-ma-xl">
+              <card-map/>
+          </div>
+        </div>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "@vue/composition-api";
+import CardMap from "src/components/CardMap.vue";
+import GoogleMaps from "src/components/GoogleMaps.vue";
+import InputGenericoHome from "src/components/InputGenericoHome.vue";
+
+export default defineComponent({
+  components: { InputGenericoHome, CardMap },
+  setup() {},
+});
+</script>
