e50165603e6ebf026549bb0cef1c2c686bdef187
Álvaro <alvaro@jig.es>
2021-10-07 13:51:45 +0200

    cambiosrutacard

--- src/components/CardResults.vue
+++ src/components/CardResults.vue
@@ -1,114 +1,73 @@
 <template>
-  <q-list class="rounded-borders ">
-    <q-expansion-item >
-      <template v-slot:header class="wrap">
-        <q-item-section avatar style="display: inline; padding-top: 8px">
-          <img class="moneco" src="../assets/Walk.svg" />
-          <div class="box moneco">
-            <p class="numero">212</p>
-          </div>
-          <img class="moneco" src="../assets/Walk.svg" />
-        </q-item-section>
+    <q-list class="rounded-borders">
+        <q-expansion-item>
+            <template v-slot:header class="wrap">
+                <div class="row bus-route">
+                    <q-item-section avatar class="q-pr-none">
+                        <img class="moneco" src="../assets/Walk.svg" />
+                        <div class="box moneco">
+                            <p class="numero">212</p>
+                        </div>
+                        <img class="moneco" src="../assets/Walk.svg" />
+                    </q-item-section>
 
-        <q-item-section style="align-items: end">
-          <div class="row items-center" style="margin-top: -6px">
-            <div style="display: block">
-              <p class="headerNumber">{{ Min }}</p>
-              <legend style="margin-top: -7px">MIN</legend>
-            </div>
-          </div>
-        </q-item-section>
+                    <q-item-section class="time-route" style="">
+                        <p class="headerNumber">{{ Min }}</p>
+                        <legend style="">MIN</legend>
+                    </q-item-section>
+                </div>
 
-        <div class="row" style="width: 100%">
-          <div style="width: 100%" class="col col-lineheader"></div>
-        </div>
+                <div class="row" style="width: 100%; margin-top: 10px">
+                    <div class="col-md-4" style="width: 33%">
+                        <legend>
+                            DEPART
+                            <span class="date">
+                                {{ Depart }}
+                                <img src="../assets/Group55.svg" style="margin-left: 8px"
+                            /></span>
+                        </legend>
+                    </div>
 
-        <div class="row" style="width: 100%; margin-top: 10px">
-          <div class="col-md-4" style="width: 33%">
-            <legend>
-              DEPART
-              <span class="date">
-                {{ Depart }}
-                <img src="../assets/Group55.svg" style="margin-left: 8px"
-              /></span>
-            </legend>
-          </div>
+                    <div class="col-md-4" style="width: 33%">
+                        <legend>
+                            ARRIVE
+                            <span class="date"> {{ Arrive }} </span>
+                        </legend>
+                    </div>
 
-          <div class="col-md-4" style="width: 33%">
-            <legend>
-              ARRIVE
-              <span class="date"> {{ Arrive }} </span>
-            </legend>
-          </div>
+                    <div class="col-md-4" style="width: 33%">
+                        <legend style="width: 100%; display: flex; justify-content: right">
+                            <span style="display: flex" class="date">
+                                {{ Price }} €
+                                <img src="../assets/Info.svg" style="margin-left: 4px" />
+                            </span>
+                        </legend>
+                    </div>
+                </div>
+            </template>
 
-          <div class="col-md-4" style="width: 33%">
-            <legend style="width: 100%; display: flex; justify-content: right">
-              <span style="display: flex" class="date">
-                {{ Price }} €
-                <img src="../assets/Info.svg" style="margin-left: 4px" />
-              </span>
-            </legend>
-          </div>
-        </div>
-      </template>
+            <q-card>
+                <q-card-section>
+                    <div class="logos">
+                        <div class="row" style="width: 100%">
+                            <div style="width: 100%; margin-top: -16px" class="col col-lineheader"></div>
+                        </div>
+                        <img src="../assets/Download.svg" style="margin-right: 10px" />
+                        <img src="../assets/Link.svg" style="margin-right: 10px" />
+                        <img src="../assets/Print.svg" />
+                    </div>
+                </q-card-section>
 
-      <q-card>
-        <q-card-section>
-          <div class="logos">
-            <div class="row" style="width: 100%">
-              <div
-                style="width: 100%; margin-top: -16px"
-                class="col col-lineheader"
-              ></div>
-            </div>
-            <img src="../assets/Download.svg" style="margin-right: 10px" />
-            <img src="../assets/Link.svg" style="margin-right: 10px" />
-            <img src="../assets/Print.svg" />
-          </div>
-        </q-card-section>
+                <card-results-line Hour="12:20" Time="9" Distance="520" BusStop="Leonardo Hotel Wolfsburg City Center" Walk="true" />
+                <card-results-line Hour="12:29" Line="201" Color="#D41217" Number="201" BusStop="Kuntsmuseum" BusStops="7" Time="7" />
+                <card-results-line Hour="12:36" Line="212" BusStop="ZOB Bay 8" BusStops="1" Number="212" Time="2" Color="#F07E12" />
+                <card-results-line Hour="12:38" Walk="true" BusStop="Feuerwehr" Distance="300" Time="6" />
+                <card-results-line Hour="12:38" Final="true" BusStop="Volkswagen AutoMuseum" />
 
-        <card-results-line
-          Hour="12:20"
-          Time="9"
-          Distance="520"
-          BusStop="Leonardo Hotel Wolfsburg City Center"
-          Walk="true"
-        />
-        <card-results-line
-          Hour="12:29"
-          Line="201"
-          Color="#D41217"
-          Number="201"
-          BusStop="Kuntsmuseum"
-          BusStops="7"
-          Time="7"
-        />
-        <card-results-line
-          Hour="12:36"
-          Line="212"
-          BusStop="ZOB Bay 8"
-          BusStops="1"
-          Number="212"
-          Time="2"
-          Color="#F07E12"
-        />
-        <card-results-line
-          Hour="12:38"
-          Walk="true"
-          BusStop="Feuerwehr"
-          Distance="300"
-          Time="6"
-        />
-        <card-results-line
-          Hour="12:38"
-          Final="true"
-          BusStop="Volkswagen AutoMuseum"
-        />
-
-        <q-card-section> </q-card-section>
-      </q-card>
-    </q-expansion-item>
-  </q-list>
+                <q-card-section> </q-card-section>
+            </q-card>
+        </q-expansion-item>
+    </q-list>
 </template>
 
 <script>
@@ -116,155 +75,176 @@ import { defineComponent } from "@vue/composition-api";
 import CardResultsLine from "./CardResultsLine.vue";
 
 export default defineComponent({
-  components: { CardResultsLine },
-  props: {
-    Min: String,
-    Depart: String,
-    Arrive: String,
-    Price: String,
-  },
-  setup() {},
+    components: { CardResultsLine },
+    props: {
+        Min: String,
+        Depart: String,
+        Arrive: String,
+        Price: String,
+    },
+    setup() {},
 });
 </script>
 
+<style lang="scss">
+.q-list {
+    transition: all ease-in-out 0.2s;
 
-
-<style>
-body.desktop .q-focus-helper {
-  background: none;
-  opacity: 1;
-  border: 1px solid rgba(235, 46, 45, 0.6);
-  top: 0;
-  left: 0;
-  border-radius: 16px;
-  transition: all ease-in-out 0.3s;
-  pointer-events: none;
+    &:hover {
+        background: #fff;
+        opacity: 1;
+        margin-top: 18px;
+        margin-bottom: -2px;
+        box-shadow: 0px 3px 6px rgba(17, 17, 17, 0.1);
+    }
 }
 
+.bus-route {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    height: 30px;
+
+    .q-item__section {
+        display: flex;
+        align-items: center;
+        flex-direction: inherit;
+    }
+
+    .time-route {
+        flex: none;
+        justify-content: center;
+    }
+}
 
 body.desktop .q-focus-helper {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  pointer-events: none;
-  border-radius: inherit;
-  opacity: 0;
-  transition: all ease-in-out 0.3s;
-  border-radius: 16px;
-  overflow: hidden;
+    background: none;
+    opacity: 1;
+    top: 0;
+    left: 0;
+    border-radius: 16px;
+    transition: all ease-in-out 0.3s;
+    pointer-events: none;
+}
+
+body.desktop .q-focus-helper {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    pointer-events: none;
+    border-radius: inherit;
+    opacity: 0;
+    transition: all ease-in-out 0.3s;
+    border-radius: 16px;
+    overflow: hidden;
 }
 
 body.desktop .q-focusable:focus > .q-focus-helper,
 body.desktop .q-manual-focusable--focused > .q-focus-helper,
 body.desktop .q-hoverable:hover > .q-focus-helper {
-  background: none;
-  opacity: 1;
-  border-bottom-left-radius: 0;
-  border-bottom-right-radius: 0;
+    background: none;
+    opacity: 1;
 }
 
 body.desktop .q-focus-helper::before {
-  background: none;
-  width: 1px;
-  background: #d92127;
-  height: 3px;
-  margin: 0 50%;
-  transition: all ease-in-out 0.3s;
-  top: initial;
-  bottom: 0;
+    background: none;
+    width: 1px;
+    background: #d92127;
+    height: 3px;
+    margin: 0 50%;
+    transition: all ease-in-out 0.3s;
+    top: initial;
+    bottom: 0;
+    display: none;
 }
 
 body.desktop .q-focusable:focus > .q-focus-helper::before,
 body.desktop .q-manual-focusable--focused > .q-focus-helper::before,
 body.desktop .q-hoverable:hover > .q-focus-helper::before {
-  opacity: 1;
-  width: 100%;
-  margin: 0;
+    opacity: 1;
+    width: 100%;
+    margin: 0;
+    display: none;
 }
 
 body.desktop .q-focusable:focus > .q-focus-helper::after,
 body.desktop .q-manual-focusable--focused > .q-focus-helper::after,
 body.desktop .q-hoverable:hover > .q-focus-helper::after {
-  opacity: 0;}
+    opacity: 0;
+    display: none;
+}
 
 .col-lineheader::before {
-  content: "";
-  display: inline-block;
+    content: "";
+    display: inline-block;
 
-  width: 100%;
+    width: 100%;
 
-  border: 1px dashed #f2f2f2;
+    border: 1px dashed #f2f2f2;
 }
 
 .mxrig {
-  width: 100%;
-  float: right;
+    width: 100%;
+    float: right;
 }
 
 .center {
-  justify-content: center;
-  align-items: center;
-  display: flex;
+    justify-content: center;
+    align-items: center;
+    display: flex;
 }
 
 legend {
-  font-family: Source Sans Pro;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 12px;
-  line-height: 15px;
-  /* identical to box height */
-
-  /* Neutral (Dark)/40 */
-
-  color: #9a9a9a;
+    font-family: Source Sans Pro;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 12px;
+    line-height: 15px;
+    color: #9a9a9a;
 }
 .date {
-  color: #282828;
-  font-size: 14px;
+    color: #282828;
+    font-size: 14px;
 }
 
 .no-wrap {
-  flex-wrap: wrap !important;
+    flex-wrap: wrap !important;
 }
 .moneco {
-  float: left;
+    float: left;
 }
 .rounded-borders {
-  background: #ffffff;
-  /* Shadow/Shadow Card Form */
+    background: #ffffff;
 
-  box-shadow: 0px 8px 16px rgba(17, 17, 17, 0.06);
-  border-radius: 16px;
+    box-shadow: 0px 8px 16px rgba(17, 17, 17, 0.06);
+    border-radius: 16px;
 }
 
 .q-expansion-item__toggle-icon {
-  display: none;
+    display: none;
 }
 
 .headerNumber {
-  font-family: Source Sans Pro;
-  font-style: normal;
-  font-weight: bold;
-  font-size: 21px;
-  line-height: 140%;
-  /* or 29px */
+    font-family: Source Sans Pro;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 21px;
+    line-height: 140%;
 
-  text-align: center;
+    text-align: center;
 
-  /* Neutral (Dark)/80 */
-
-  color: #282828;
+    color: #282828;
 }
 
 .box {
-  background: #f07e12;
-  border-radius: 8px;
-  width: 40px;
-  height: 26px;
-  text-align: center;
-  justify-content: center;
+    background: #f07e12;
+    border-radius: 8px;
+    width: 40px;
+    height: 26px;
+    text-align: center;
+    justify-content: center;
 }
 </style>
