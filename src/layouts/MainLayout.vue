8d478dd5624e7d413e459702a9602acf46e092eb
jocarrd <jocarrd@unirioja.es>
2021-12-14 11:16:04 +0000

    Fullscreennav

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -8,9 +8,9 @@
                 <div class="header-icons">
                     <a href="#"> <img src="../assets/cil_language.svg" /></a>
                     <a href="#"> <img src="../assets/mdi_checkbox-blank-badge-outline.svg" /> </a>
-                    <q-btn  flat round @click="$q.dark.toggle()" :icon="$q.dark.isActive ? 'nights_stay' : 'wb_sunny'"/>
+                    <q-btn size="16px" flat round @click="$q.dark.toggle()" :icon="$q.dark.isActive ? 'nights_stay' : 'wb_sunny'"/>
                     <a class="menucabecera" href="#">
-                        <svg width="24" height="40" viewBox="0 0 24 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <svg  :onclick="openNav" width="24" height="40" viewBox="0 0 24 40" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <g clip-path="url(#clip0_1693:1640)">
                                 <path d="M18 5L18 35L17 35L17 5L18 5ZM13 5L13 35L12 35L12 5L13 5ZM8 5L8 35L7 35L7 5L8 5Z" fill="#282828" />
                             </g>
@@ -24,6 +24,15 @@
                 </div>
             </q-toolbar>
         </q-header>
+        <div id="myNav" class="overlay">
+            <a href="javascript:void(0)" class="closebtn" :onclick="closeNav">&times;</a>
+            <div class="overlay-content">
+                <a href="#">About</a>
+                <a href="#">Services</a>
+                <a href="#">Clients</a>
+                <a href="#">Contact</a>
+            </div>
+        </div>
 
         <q-page-container>
             <router-view />
@@ -76,6 +85,8 @@
 </template>
 
 <script>
+
+
 import { ref } from "vue";
 
 export default {
@@ -95,10 +106,74 @@ export default {
             },
         };
     },
+    methods:{
+         openNav: function() {
+  document.getElementById("myNav").style.width = "100%";
+},
+
+ closeNav: function() {
+  document.getElementById("myNav").style.width = "0%";
+}
+    },
+    
 };
+
+
 </script>
 
 <style scoped lang="scss">
+
+.overlay .closebtn {
+  position: absolute;
+  top: 20px;
+  right: 45px;
+  font-size: 60px;
+}
+
+@media screen and (max-height: 450px) {
+  .overlay a {font-size: 20px}
+  .overlay .closebtn {
+  font-size: 40px;
+  top: 15px;
+  right: 35px;
+  }
+}
+
+.overlay {
+  height: 100%;
+  width: 0;
+  position: fixed;
+  z-index: 2001;
+  top: 0;
+  left: 0;
+  background-color: rgb(0,0,0);
+  background-color: rgba(0,0,0, 0.9);
+  overflow-x: hidden;
+  transition: 0.5s;
+}
+.overlay-content {
+  position: relative;
+  top: 25%;
+  width: 100%;
+  text-align: center;
+  margin-top: 30px;
+}
+
+.overlay a {
+  padding: 8px;
+  text-decoration: none;
+  font-size: 36px;
+  color: #818181;
+  display: block;
+  transition: 0.3s;
+}
+
+
+.overlay a:hover, .overlay a:focus {
+  color: #f1f1f1;
+}
+
+
 /* START header */
 .q-toolbar {
     max-width: 1440px;
